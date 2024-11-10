14944166f6122f5835b53b5be13eab7c4a9653a8
Asher <124173983+99lash@users.noreply.github.com>
2024-11-10 09:10:36 +0800

    updated leonardo

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello, Git!");
     System.out.println("Hello, World!");
-    System.out.println("Added a leonardo branch!");
+    System.out.println("Added a leonardo branch!!!");
+    System.out.println("Remve ttangina mo");
   }
 }
